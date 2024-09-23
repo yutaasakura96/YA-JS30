47cbc54ca7fafa376b216ac65b127f46ce5ef2f8
yuta asakura <yuta.asakura.se@gmail.com>
2024-09-23 20:27:47 +0900

    Added getjokes functionality using JOKE API

--- project12/script.js
+++ project12/script.js
@@ -106,17 +106,39 @@ const VoiceRSS = {
 };
 
 // VOICE RSS API Text to Speech Functionality
-function test() {
-  VoiceRSS.speech({
-    key: "Your VOICERSS API KEY HERE",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
-}
+// function test() {
+//   VoiceRSS.speech({
+//     key: "YOUR VOICERSS API KEY HERE",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
 
-test();
+// test();
+
+// Get Jokes from JOKE API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    console.log(joke);
+  } catch (error) {
+    // Catch Errors Here
+    console.log("Error", error);
+  }
+}
+getJokes();
