5fdf02a5daf6f53f0da3b0597b32c1a36ae83153
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-03 23:05:37 +0900

    added save favorites functionality

--- project15/script.js
+++ project15/script.js
@@ -10,6 +10,7 @@ const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 // Update the DOM
 function updateDOM() {
@@ -39,6 +40,7 @@ function updateDOM() {
     const saveText = document.createElement("p");
     saveText.classList.add("clickable");
     saveText.textContent = "Add To Favorites";
+    saveText.setAttribute("onclick", `saveFavorite('${result.url}')`);
     // Card Text
     const cardText = document.createElement("p");
     cardText.textContent = result.explanation;
@@ -74,6 +76,23 @@ async function getNasaPictures() {
     // Catch Error Here
     console.error("Error fetching NASA pictures:", error);
   }
+}
+
+// Add Result to Favorites
+function saveFavorite(itemUrl) {
+  // Loop through Results Array to select Favorite
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      // Show Save Confirmation for 2 seconds
+      saveConfirmed.hidden = false;
+      setTimeout(() => {
+        saveConfirmed.hidden = true;
+      }, 2000);
+      // Set Favorites in localStorage
+      localStorage.setItem('nasaFavorites', JSON.stringify(favorites))
+    }
+  });
 }
 
 // On Load
