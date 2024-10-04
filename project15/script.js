9a0c6031604361254fc27b8268b09eb70ca237e0
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-04 22:16:56 +0900

    added getting favorites from localStorage and rebuilding the DOM using that data

--- project15/script.js
+++ project15/script.js
@@ -12,9 +12,13 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-// Update the DOM
-function updateDOM() {
-  resultsArray.forEach((result) => {
+// Build the DOM nodes
+
+function createDOMNodes(page) {
+  const currentArray =
+    page === "results" ? resultsArray : Object.values(favorites);
+  console.log("Current Array", page, currentArray);
+  currentArray.forEach((result) => {
     // Card Container
     const card = document.createElement("div");
     card.classList.add("card");
@@ -64,14 +68,23 @@ function updateDOM() {
   });
 }
 
+// Update the DOM
+function updateDOM(page) {
+  // Get Favorites from localStorage
+  if (localStorage.getItem("nasaFavorites")) {
+    favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
+    console.log("Favorites from local storage", favorites);
+  }
+  createDOMNodes(page);
+}
+
 // Get 10 Images from NASA APOD API
 
 async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    console.log(resultsArray);
-    updateDOM();
+    updateDOM("favorites");
   } catch (error) {
     // Catch Error Here
     console.error("Error fetching NASA pictures:", error);
@@ -90,7 +103,7 @@ function saveFavorite(itemUrl) {
         saveConfirmed.hidden = true;
       }, 2000);
       // Set Favorites in localStorage
-      localStorage.setItem('nasaFavorites', JSON.stringify(favorites))
+      localStorage.setItem("nasaFavorites", JSON.stringify(favorites));
     }
   });
 }
